c7f945e217ad5404a3935277dbecf2352b5caa9a
Renukumar J <renukumar@ncssoft.in>
2026-06-29 18:03:37 +0530

    Initial Playwright Project

---
+++ src/main/java/Playwrigh/launch.java
@@ -0,0 +1,26 @@
+package Playwrigh;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class launch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Playwright playwright=Playwright.create();
+		Browser browser=playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		  
+		Page page=browser.newPage();
+		page.navigate("https://letcode.in/edit");
+		String Pgtitle=page.title();
+		System.out.println("page title "+Pgtitle);
+		page.close();
+		browser.close();
+		playwright.close();
+		
+	}
+
+}
